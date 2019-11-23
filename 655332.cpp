48ca07b1b72b5d12adedb2fd30dda9ac3c605cbe
小杜 <57487152+tutu1314520@users.noreply.github.com>
2019-11-23 18:19:49 +0800

    Update 655332.cpp

--- 655332.cpp
+++ 655332.cpp
@@ -9,12 +9,9 @@ int main(void) {
        } while(times < 1 || times > 8);
 
        for(int t = 0; t < times;t++) {
-           for(int j = 1;j <= 5 - t;j++){
-	       cout << " ";
-           }
-           for(int n = 0;n <= t;n++){
-	       cout << "#";
+           for (int n = 0; n <= t;n++){
+                cout << "#";
            }
            cout << "\n";
-       }       
+       }      
 }// 272 32 杜宸緯 655332
