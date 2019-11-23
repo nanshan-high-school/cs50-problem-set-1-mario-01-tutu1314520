8f4be212878433850225acb4b71414df20f7b55c
小杜 <57487152+tutu1314520@users.noreply.github.com>
2019-11-23 18:15:40 +0800

    Update 655332.cpp

--- 655332.cpp
+++ 655332.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(void) {
     int times;
     do {
         cout << "高度:";
         cin >> times;
        } while(times < 1 || times > 8);
+
        for(int t = 0; t < times;t++) {
-           for (int n = 0; n <= t;n++){
-                cout << "*";
-            }
-            cout << "\n";
-        }
-    } //272 32 杜宸緯 655332
+           for(int j=1;j<=5-t;j++){
+		       cout << " ";
+		       }
+		       for(int n=0;n<=t;n++){
+		       cout << "#";
+		       }
+           cout << "\n";
+       }       
+}// 272 32 杜宸緯 655332
